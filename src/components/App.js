8be15d858ad5ea6888d7d6f7bc15c8dc70ef8c45
Axel Torrico <torricohernan2@gmail.com>
2023-02-06 17:50:08 -0300

    cambio en el codigo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,94 @@
 import React from "react";
-import { TodoProvider } from "../TodoContext";
-import { AppUI } from "../AppUI";
+import { useTodos } from "./useTodos";
+import { TodoHeader } from '../components/TodoHeader';
+import { TodoTitle } from '../components/TodoTitle.js';
+import { TodoCounter } from '../components/TodoCounter.js';
+import { TodoSearch } from "../components/TodoSearch.js";
+import { TodoList } from "../components/TodoList.js";
+import { TodoItem } from "../components/TodoItem.js";
+import { TodosError } from '../components/TodosError.js';
+import { TodosLoading } from '../components/TodosLoading.js';
+import { EmptyTodos } from '../components/EmptyTodos.js';
+import { TodoForm } from '../components/TodoForm.js';
+import { CreateTodoButton } from "../components/CreateTodoButton.js";
+import { Modal } from '../Modal/index.js';
 
 function App() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    addTodo,
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader loading={loading}>
+        <TodoTitle />
+        <TodoCounter
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
+
+      <TodoList
+        error={error}
+        loading={loading}
+        totalTodos={totalTodos}
+        searchedTodos={searchedTodos}
+        searchText={searchValue}
+        //render props
+        onError={() => <TodosError />}
+        onLoading={() => <TodosLoading />}
+        onEmptyTodos={() => <EmptyTodos />}
+        onEmptySearchResults={(searchText) => <p>No hay resultados para {searchText}</p>}
+        //render functions
+        render={todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )}
+      >
+        {/* {todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )} */}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm
+            addTodo={addTodo}
+            setOpenModal={setOpenModal}
+          />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,26 @@
-import react from "react";
+import React from "react";
 import '../css/TodoList.css';
 
-function TodoList (props) {
+function TodoList(props) {
+  const renderFunc = props.children || props.render;
+
   return(
-      <section>
-          <ul>
-              {props.children}
-          </ul>
-      </section>
+    <section className="TodoList-container">
+      {props.error && props.onError()}
+      {props.loading && props.onLoading()}
+
+      {/* si no hay nada en loading y ademas no hay nada en totalTodos, entonces voy a renderizar lo que venga en onEmptyTodos() */}
+      {(!props.loading && !props.totalTodos) && props.onEmptyTodos()}
+
+      {/* si es true que totalTodos no es 0, null o undefined y ademas la longitud de searchedTodos es 0, entonces voy a renderizar lo que venga en onEmptySearchResults() */}
+      {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
+
+      {props.searchedTodos.map(renderFunc)}
+
+      <ul>
+        {props.children}
+      </ul>
+    </section>
   );
 }
 
