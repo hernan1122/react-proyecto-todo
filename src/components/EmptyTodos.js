53088e4f7f9d565d1466339b10751b18cbbe40a7
Axel Torrico <torricohernan2@gmail.com>
2023-02-06 11:52:49 -0300

    cambio de texto en emptytodos

--- src/components/EmptyTodos.js
+++ src/components/EmptyTodos.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../css/EmptyTodos.css'
 
 function EmptyTodos() {
-  return <p>¡Crea un nuevo TODO!</p>
+  return <p>VAMOS... <br/> ¡Crea un nuevo TODO!</p>
 }
 
 export { EmptyTodos };
